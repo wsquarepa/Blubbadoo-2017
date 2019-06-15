a995efe6ddeff4326c6d29d38c8b05f481e0897f
Willy <wsquarepa@gmail.com>
2019-06-15 10:17:29 -0700

    added a warnings system (In beta)

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var fs = require("fs")
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -20,7 +21,9 @@ bot.on('ready', function (evt) {
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     //Checking if is bot himself
-    if (userID == 584567403166433280) {
+    if (userID == 584567403166433280 && message.substring(0,4) == 'warn') {
+        
+    } else if (userID == 584567403166433280){
         return;
     }
     //DA prefix
@@ -69,6 +72,37 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: "Failed to Ban. \n ```Reason: Permission Error```"
                 })
                 break;
+            case 'warnings':
+                if (userID == 509874745567870987) {
+                    fs.readFile("warnings.txt", function(err, buf) {
+                        var hashcode = buf.toString()
+                        var userWarnings = {}
+
+                        var user = 0
+                        var warnings = 0
+
+                        for (item in hashcode) {
+                            user, warnings = item.split(':')
+                            userWarnings[user] = warnings
+                        }
+
+                        bot.sendMessage({
+                            to:channelID,
+                            message: 'You have ' + userWarnings[userID] + ' warnings.'
+                        })
+                    });
+                }
+            case 'warn':
+                if (userID == 509874745567870987) {
+                    bot.sendMessage({
+                        to:channelID,
+                        message: 'Did you mean '
+                    })
+                    bot.sendMessage({
+                        to:channelID,
+                        message: 'warn ' + args[0] + ' ' + args[1]
+                    })
+                }
             // More case commands above
          }
      } else if ((message.substring(0,5) == 'henlo') || (message.substring(0,5) == 'hello')){
@@ -81,7 +115,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
              to:channelID,
              message: 'Blubbadoo!'
          })
-     } else if (message.substring(0,4) == 'warn') {
+     } else if (message.substring(0,4) == 'warn' && userID == 509874745567870987) {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         args = args.splice(1);
@@ -97,10 +131,27 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             return;
         }
 
+        var users = {}
+
+        fs.readFile("warnings.txt", function(err, buf) {
+            var hashcode = buf.toString()
+            users = hashcode.split('\n')
+        });
+
+        console.log(users);
+
          bot.sendMessage({
              to:channelID,
              message: '<@' + args[0] + '>' + ' has been warned. \n ```Reason: ' + args[1] + ' ```'
          })
+
+         var warnedUser = args[0];
+
+         fs.appendFile("warnings.txt", (warnedUser + ':' + args[1] + '\n'), (err) => {
+            if (err) console.log(err);
+            console.log("Successfully Written to File.");
+          });
+
      } else if (message.substring(0,3) == 'tis') {
          bot.sendMessage({
              to:channelID,
